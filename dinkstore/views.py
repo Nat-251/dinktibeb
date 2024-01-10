750fdaec1a26c7d3e30dadda88df6d1c110f507d
Nat-251 <natygizachew@gmail.com>
2024-01-10 16:18:49 +0300

    Product Image Field

--- dinkstore/views.py
+++ dinkstore/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from .models import * 
+
+# create your view here. 
 
 def store(request):
-     context = {}
-     return render(request, 'dinkstore/store.html', context)
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'dinkstore/store.html', context)
 
 def cart(request):
-     context = {}
-     return render(request, 'dinkstore/cart.html', context)
+	context = {}
+	return render(request, 'dinkstore/cart.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'dinkstore/checkout.html', context)
+	context = {}
+	return render(request, 'dinkstore/checkout.html', context)
