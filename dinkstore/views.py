bfc20ee2c979327eefd34c211b035a86f48f3cbd
Nat-251 <natygizachew@gmail.com>
2024-01-15 19:14:43 +0300

    Add to cart

--- dinkstore/views.py
+++ dinkstore/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 from .models import * 
 
 # create your view here. 
@@ -32,3 +34,7 @@ def checkout(request):
 
 	context = {'items':items, 'order':order}
 	return render(request, 'dinkstore/checkout.html', context)
+
+
+def updateItem(request):
+	return JsonResponse('Item was added', safe=False)
